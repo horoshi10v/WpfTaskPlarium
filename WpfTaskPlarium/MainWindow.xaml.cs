31b282fecd909507d4f22c39c23d79ef36d405db
V <V@DESKTOP-VJ3RFFD>
2021-10-16 04:42:27 +0300

    Добавьте файлы проекта.

---
+++ WpfTaskPlarium/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTaskPlarium
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPoli_Click(object sender, RoutedEventArgs e)
+        {
+            PolindromWindow polindromWindow = new PolindromWindow();
+            polindromWindow.Owner = this;
+            polindromWindow.Show();
+        }
+
+        private void buttonSort_Click(object sender, RoutedEventArgs e)
+        {
+            SortWindow sortWindow = new SortWindow();
+            sortWindow.Owner = this;
+            sortWindow.Show();
+        }
+
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxButton Button = MessageBoxButton.YesNo;
+            MessageBoxResult Result = MessageBox.Show("Вы уверены что хотите выйти из приложения?", "Выход", Button);
+            if (Result == MessageBoxResult.Yes)
+                System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
---
+++ WpfTaskPlarium/PolindromWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTaskPlarium
+{
+    /// <summary>
+    /// Логика взаимодействия для PolindromWindow.xaml
+    /// </summary>
+    public partial class PolindromWindow : Window
+    {
+        public PolindromWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int inputNum = isDigit(textBox.Text);
+            int r, sum = 0, temp;
+            temp = inputNum;
+
+            while (inputNum > 0)
+            {
+                r = inputNum % 10;
+                sum = (sum * 10) + r;
+                inputNum = inputNum / 10;
+            }
+
+            if (temp == sum)
+                MessageBox.Show("Число является полиндромом");
+            else
+                MessageBox.Show("Число не является полиндромом");
+        }
+
+        public static int isDigit(string str)
+        {
+            if (str.Any(c => char.IsLetter(c)) || str == "")
+            {
+                if (str != "")
+                    MessageBox.Show("Вводить можно только числа");
+                return -1;
+            }
+            else
+                return (int)uint.Parse(str);
+        }
+    }
+}
